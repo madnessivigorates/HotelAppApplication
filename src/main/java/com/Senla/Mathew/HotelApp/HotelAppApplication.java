590c115763409eb6fbc040a421889d8a348354a0
Matvey Lyudkevich <magicvlogs9@gmail.com>
2025-01-07 22:13:41 +0300

    add: Spring Security and Tests

--- src/main/java/com/Senla/Mathew/HotelApp/HotelAppApplication.java
+++ src/main/java/com/Senla/Mathew/HotelApp/HotelAppApplication.java
@@ -2,10 +2,12 @@ package com.Senla.Mathew.HotelApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @SpringBootApplication
 public class HotelAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelAppApplication.class, args);
 	}
+
 }
